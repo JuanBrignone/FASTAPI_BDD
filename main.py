45014eb5b0b69e3f6efee484eda7e37db0b0f595
JuanBrignone <juan.brignone@correo.ucu.edu.uy>
2024-11-04 18:18:16 -0300

    endponits actividades

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, SessionLocal
 from models import Actividad, Turno, Base
-from schemas import TurnoPost
+from schemas import TurnoPost, ActividadPost, ActividadResponse, ActividadUpdate
 
 app = FastAPI()
 
@@ -14,6 +14,10 @@ def get_db():
         db.close()
 
 
+#############################################################################################
+#                               ACTIVIDADES                                                 #
+#############################################################################################
+
 #Trae todas las actividades
 @app.get("/actividades")
 async def read_actividades(db: Session = Depends(get_db)):
@@ -21,6 +25,55 @@ async def read_actividades(db: Session = Depends(get_db)):
     if not actividades:
         raise HTTPException(status_code=404, detail="No hay actividades disponibles")
     return actividades
+
+#Agregar actividades
+@app.post("/actividades")
+async def post_actividad(actividad: ActividadPost, db: Session = Depends(get_db)):
+    nueva_actividad = Actividad(
+        nombre=actividad.nombre, 
+        descripcion=actividad.descripcion,
+        costo=actividad.costo
+        )
+    db.add(nueva_actividad)
+    db.commit()
+    db.refresh(nueva_actividad)
+    return nueva_actividad
+
+
+#Eliminar actividades
+@app.delete("/actividades/{id_actividad}", status_code=200)
+async def delete_actividad(id_actividad: int, db:Session = Depends(get_db)):
+    actividad = db.query(Actividad).filter(Actividad.id_actividad == id_actividad).first()
+    if not actividad:
+        raise HTTPException(status_code=404, detail="No se encontro un turno para eliminar")
+    db.delete(actividad)
+    db.commit()
+    return {"detail": "Actividad eliminada correctamente"}
+
+
+#Modificar actividad
+@app.put("/actividades/{id_actividad}", response_model=ActividadResponse)
+async def update_actividad(id_actividad: int, actividad: ActividadUpdate, db: Session = Depends(get_db)):
+    db_actividad = db.query(Actividad).filter(Actividad.id_actividad == id_actividad).first()
+    if not db_actividad:
+        raise HTTPException(status_code=404, detail="Actividad no encontrada")
+    
+    if actividad.nombre is not None:
+        db_actividad.nombre = actividad.nombre
+    if actividad.descripcion is not None:
+        db_actividad.descripcion = actividad.descripcion
+    if actividad.costo is not None:
+        db_actividad.costo = actividad.costo
+
+    db.commit()
+    db.refresh(db_actividad)
+
+    return db_actividad
+
+
+#############################################################################################
+#                               TURNOS                                                      #
+#############################################################################################
 
 
 #Trae todos los turnos disponibles
@@ -32,18 +85,18 @@ async def read_turnos(db: Session = Depends(get_db)):
     return turnos
 
 
-#Añadir más turnos
+#Agregar turnos
 @app.post("/turnos")
 async def post_turno(turno: TurnoPost, db: Session = Depends(get_db)):
-    db_turno = Turno(hora_inicio=turno.hora_inicio, hora_fin=turno.hora_fin)
-    db.add(db_turno)
+    nuevo_turno = Turno(hora_inicio=turno.hora_inicio, hora_fin=turno.hora_fin)
+    db.add(nuevo_turno)
     db.commit()
-    db.refresh(db_turno)
-    return db_turno
+    db.refresh(nuevo_turno)
+    return nuevo_turno
 
 
 #Eliminar turnos
-@app.delete("/turnos/{id_turno}", status_code=204)
+@app.delete("/turnos/{id_turno}", status_code=200)
 async def delete_turno(id_turno: int, db:Session = Depends(get_db)):
     turno = db.query(Turno).filter(Turno.id_turno == id_turno).first()
     if not turno:
@@ -51,3 +104,9 @@ async def delete_turno(id_turno: int, db:Session = Depends(get_db)):
     db.delete(turno)
     db.commit()
     return {"detail": "Turno eliminado correctamente"}
+
+
+#############################################################################################
+#                               ALUMNOS                                                     #
+#############################################################################################
+
--- schemas.py
+++ schemas.py
@@ -1,6 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import time
+
 
 class TurnoPost(BaseModel):
     hora_inicio: time
     hora_fin: time
+
+class ActividadPost(BaseModel):
+    nombre: str
+    descripcion: str
+    costo: float
+
+
+#estructura que se utiliza para la respuesta al cliente
+class ActividadResponse(BaseModel):
+    id_actividad: int
+    nombre: str
+    descripcion: str
+    costo: float
+
+#estructura de los campos en la solicitud para actualizar una actividad
+class ActividadUpdate(BaseModel):
+    nombre: str = Field(None, max_length=100)
+    descripcion: str = Field(None, max_length=255)
+    costo: float = Field(None, gt=0)
